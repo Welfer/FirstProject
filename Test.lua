01a7f33785f2b26c87debc056e7fe91b169b7040
João Victor C. Lima <Joo Victor C. Lima>
2016-09-10 19:57:12 -0300

    muitas mudanças

--- Test.lua
+++ Test.lua
@@ -1,1 +1,39 @@
-print("Hello World versao 2")
+function avaliar( n,q,w,e )
+	print("sua media eh:"..media( n,q,w,e ))
+	if media( n,q,w,e )<7 then
+		return "reprovado"
+	else 
+		return "aprovado"
+	end
+end
+function media( n,q,w,e )
+	return (n+q+w+e)/4
+end
+n1,n2,n3,n4=0,0,0,0
+print("Digite as notas das provas!")
+n1,n2,n3,n4=io.read ("*number"),io.read ("*number"),io.read ("*number"),io.read ("*number")
+print("seu status eh "..avaliar( n1,n2,n3,n4 ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
